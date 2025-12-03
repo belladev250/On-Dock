c1566c59f60b18894e91752b131b8f6a4d3620c3
Pixeleye Blue <pixeleyeblue@Blue-PixelEye-iMac.local>
2025-12-03 17:14:15 +0200

    onboarding the app

---
+++ lib/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class AppColors {
+  
+  // Primary colors
+  static const Color darkPurple = Color(0xFF101624);
+  static const Color purple = Color(0xFF6950F8); 
+  static const Color accent = Color(0xFFFFFFFF); 
+  
+  // Neutral colors
+  static const Color lightred = Color(0xFFFD7F82);
+  static const Color light = Color(0xFFF9FAFB);
+  static const Color gray = Color(0xFF006434);
+  static const Color orange = Color(0xFFD4561F);
+  static const Color maroon = Color(0xFF9C042B);
+  static const Color red = Color(0xFFEB1539);
+  static const Color lightOrange = Color(0xFFF6C58E);
+  static const Color lightGreen = Color(0xFF00AC55);
+  static const Color lightPurple = Color(0xFFB0ACFA);
+  static const Color darkGrey = Color(0xFF4B4B4B);
+
+}
